7d84ec49b1d8724b16bcc1820f5f1dddb72a7e76
frannajaya <franna.jaya@gmail.com>
2024-08-24 14:51:18 +0700

    INIT

---
+++ src/test/java/com/fran/na/jaya/virtual/thread/benchmark/project/VirtualThreadBenchmarkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fran.na.jaya.virtual.thread.benchmark.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualThreadBenchmarkProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
